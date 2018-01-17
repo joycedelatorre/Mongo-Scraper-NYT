9d7dec9dc7e19bd3a571fac3fbd4f426cbd1fcda
joyce <joycedelatorre87@gmail.com>
2018-01-17 16:58:00 -0600

    testing3 heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Article: require("./models/Article.js"),
-  Comment: require("./models/Comment.js")
+  Article: require("./Article"),
+  Comment: require("./Comment")
 };
